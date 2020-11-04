046bf50f0abd4194fd93c31bc99471392b64c9e2
Adam Young <awy6448@gmail.com>
2020-11-03 22:49:58 -0500

    no_dups completed.

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,19 @@
 # Your code here
-
-
+    
 def expensive_seq(x, y, z):
     # Your code here
+    dic = dict()
+    if ((x, y, z) not in dic):
+        dic[(x, y, z)] = mem(x, y, z)
+    return dic[(x, y, z)]
+    
+
+def mem(x, y, z):
+    if x <= 0: 
+        return y + z
+    if x >  0: 
+        return expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+
 
 
 

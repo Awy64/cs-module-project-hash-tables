4f739781484c8f4e73b795900f8bcfb82ecc0818
Adam Young <awy6448@gmail.com>
2020-11-05 19:57:25 -0500

    histo.py complete.

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,19 @@
 # Your code here
+import re
 
+with open("robin.txt") as f:
+    words = f.read()
+words = words.casefold()
+word = re.findall("(\w+)", words)
+wordDict = {}
+for a in word:
+    if a not in wordDict:
+        wordDict[a] = 1
+    else:
+        wordDict[a] += 1
+
+# for w in sorted(wordDict, key=wordDict.get, reverse=True):
+#     print(w, d[w])
+string = "#"
+for a in sorted(wordDict, key=wordDict.get, reverse=True):
+    print("%-20s %s" % (a, string * wordDict[a]))
